8912a212bd21a2504e7b04a949cd417ba3ddd330
Oliver Wright <owri753@UoA.auckland.ac.nz>
2023-08-22 14:27:07 +1200

    update: Worked on the light sensor firmware

--- PSoC Creator/PSoC Creator/Light Sensor Read Test.cydsn/main.c
+++ PSoC Creator/PSoC Creator/Light Sensor Read Test.cydsn/main.c
@@ -11,37 +11,65 @@
 */
 #include "project.h"
 
-volatile static uint8_t light1Val, light2Val;
+volatile static uint16_t light1Val, light2Val;
 
 CY_ISR(light_sens1_interrupt) {
+    // Grab timer value and reset
+    light1Val = LightSens1_TimeOut_ReadCapture();
     
+    // Reset the counter
+    uint8_t curr = TimeOutResetReg_Read();
+    TimeOutResetReg_Write(curr ^ (1 << 0));
+    
+    // Reset the interrupt flag on the pins
     LightSens1_ClearInterrupt();
 }
 
 CY_ISR(light_sens2_interrupt) {
+    // Grab timer value and reset
+    light2Val = LightSens2_TimeOut_ReadCapture();
+    // Reset the counter
+    uint8_t curr = TimeOutResetReg_Read();
+    TimeOutResetReg_Write(curr ^ (1 << 1));
     
+    // Reset the interrupt flag on the pins
     LightSens2_ClearInterrupt();
 }
 
 CY_ISR(light_sens1_overflow_interrupt) {
-
+    light1Val = 0;
 }
 
 CY_ISR(light_sens2_overflow_interrupt) {
-
+    light2Val = 0;
 }
 
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
+
+    // Enable pin interrupts
     LightSens1Edge_StartEx(light_sens1_interrupt);
-    LightSens1Edge_StartEx(light_sens2_interrupt);
+    LightSens2Edge_StartEx(light_sens2_interrupt);
+    
+    // Enable timer interrupts
+    LightSens1Overflow_StartEx(light_sens1_overflow_interrupt);
+    LightSens2Overflow_StartEx(light_sens2_overflow_interrupt);
+    
+    // Enable timers
+    LightSens1_TimeOut_Start();
+    LightSens2_TimeOut_Start();
     
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;)
     {
+        if(light1Val) {
+            DebugLED_Write(0xff);   
+        } else {
+            DebugLED_Write(0x00);   
+        }
         /* Place your application code here. */
     }
 }
