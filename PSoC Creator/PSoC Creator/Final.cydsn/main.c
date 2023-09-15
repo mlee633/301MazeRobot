0a933fd81c0f08cdde8362d03d464a307b94e957
Oliver Wright <owri753@UoA.auckland.ac.nz>
2023-09-15 19:05:58 +1200

    update: Got motor P-Controller working

--- PSoC Creator/PSoC Creator/Final.cydsn/main.c
+++ PSoC Creator/PSoC Creator/Final.cydsn/main.c
@@ -10,17 +10,13 @@
  * ========================================
 */
 #include "project.h"
+#include "uart.h"
+#include "motor.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-CY_ISR(motor1_quaddec_overflow) {
-    
-}
-
-CY_ISR(motor2_quaddec_overflow) {
-    
-}
-
-size_t FormatI8(int8_t num, char* buffer);
+char* AppendStrToStr(char *result, const char* src, size_t len);
 
 int main(void)
 {
@@ -39,55 +35,25 @@ int main(void)
 //    LightSens2_TimeOut_Start();
     
     USBUART_1_Start(0, USBUART_1_5V_OPERATION);
-    
-    QuadDec_1_Start();
-    QuadDec_2_Start();
-    
-    PWM_1_WritePeriod(255);
-    PWM_1_Start();
-    PWM_1_WriteCompare(5); // writecompare value / write period = Duty cycle percentage
-    
-    PWM_2_WritePeriod(255);
-    PWM_2_Start();
-    PWM_2_WriteCompare(5);
-    
-    QuadDec1Interrupt_StartEx(motor1_quaddec_overflow);
-    QuadDec2Interrupt_StartEx(motor2_quaddec_overflow);
-    
+    SetupMotors();
     
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;) {
-        int8_t q1 = QuadDec_1_GetCounter();
-        int8_t q2 = QuadDec_2_GetCounter();
+        float q1 = CalcMotor1Speed();
+        float q2 = CalcMotor2Speed();
         
-        char usbBuffer[12];
-        char* buff = usbBuffer;
-        buff += FormatI8(q1, buff);
-        *(buff++) = ','; *(buff++) = ' ';
-        buff += FormatI8(q2, buff);
-        *(buff++) = '\r'; *(buff++) = '\n';
         
-        while(USBUART_1_CDCIsReady() == 0) {}
-        USBUART_1_PutData(usbBuffer, buff - usbBuffer);
+        
+        FixMotorSpeeds(50);
     }
+    
 }
 
-size_t FormatI8(int8_t num, char* buffer) {
-    size_t length = 0;
-    int8_t num2 = num;
-    do { length++; num2 /= 10; } while(num2 != 0);
-    
-    bool isNegative = (num < 0);
-    char* writeLoc = buffer;
-    if(isNegative) *(writeLoc++) = '-';
-    
-    for(size_t i = 1; i <= length; i++) {
-        writeLoc[length - i] = "9876543210123456789"[9 + num % 10];
-        num /= 10;
-    }
-    
-    return length + isNegative;
+char* AppendStrToStr(char *result, const char* src, size_t len) {
+    memcpy(result, src, len);
+    return result + len;
 }
+
 
 /* [] END OF FILE */
