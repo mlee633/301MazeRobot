ab12ed4ac23137338b39268d09a1cc0537437a3f
Oliver Wright <owri753@UoA.auckland.ac.nz>
2023-09-19 15:57:13 +1200

    update: Forgot sensor files

---
+++ PSoC Creator/PSoC Creator/Final.cydsn/sensors.c
@@ -0,0 +1,20 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+#include "project.h"
+
+static volatile uint8_t sensorFlags = 0;
+
+CY_ISR(SensorRisingEdge) {
+    
+}
